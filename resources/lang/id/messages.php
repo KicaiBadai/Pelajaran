dbeae7c10830060a1e6d721889fc974ccaec9bd9
KicaiBadai <adityamaula80@gmail.com>
2024-10-26 13:11:16 +0700

    first commit

---
+++ resources/lang/id/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'login' => [
+        'title' => 'Masuk ke Akun Anda',
+        'email' => 'Alamat Email',
+        'password' => 'Kata Sandi',
+        'button' => 'Masuk',
+    ],
+    'register' => [
+        'title' => 'Buat Akun Baru',
+        'name' => 'Nama Lengkap',
+        'email' => 'Alamat Email',
+        'password' => 'Kata Sandi',
+        'confirm_password' => 'Konfirmasi Kata Sandi',
+        'button' => 'Daftar',
+    ],
+    'dashboard' => [
+        'welcome' => 'Selamat Datang di Dashboard!',
+    ],
+    'common' => [
+        'view' => 'Lihat',
+        'edit' => 'Ubah',
+        'delete' => 'Hapus',
+        'create' => 'Baru',
+        'action' => 'Opsi',
+    ],
+    'posts' => [
+        'title' => 'Judul',
+        'content' => 'Konten',
+        'newcontent' => 'Aditya Maula Wiratama ',
+        'Show' => 'Tampilkan',
+    ],
+];
